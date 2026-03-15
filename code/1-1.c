8887e8e644ea1b55a3d2851b48c4e347fa33adba
alvin7187 <alvin060718@gmail.com>
2026-03-15 18:03:11 +0900

    1-1.c 제출3

--- code/1-1.c
+++ code/1-1.c
@@ -3,11 +3,11 @@
 int main(){
     int var1 = 100;
     printf("int : %d\n",var1);
-    printf("hex : %o\n",var1);
-    printf("oct : %x\n",var1);
+    printf("hex : %x\n",var1);
+    printf("oct : %o\n",var1);
     
     char var2 = 'c';
-    printf("int : %d\n",var2);
+    printf("char : %c\n",var2);
 
     char* var3 = "hello world!";
     printf("string : %s\n", var3);

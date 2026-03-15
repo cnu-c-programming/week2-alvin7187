650bbe88b8a068eadd79493fdf0508d1b9491091
alvin7187 <alvin060718@gmail.com>
2026-03-15 17:55:08 +0900

    4-3.c 제출

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,24 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-
+    int a;
+    int isp = 0;
+    scanf("%d",&a);
+    if(a==1){
+        isp =1;
+    }
+    else{
+        for(int i = 2; i*i <= a; i++){
+            if(a%i==0){
+                isp = 1;
+                break;
+            }
+        }
+    }
+    if(isp == 0)
+        printf("true\n");
+    else
+        printf("false\n");
+    
     return 0;
 }
-

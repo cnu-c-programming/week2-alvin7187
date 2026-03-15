845a7dec5358a87202b0e65dde8cebdf66e85b54
alvin7187 <alvin060718@gmail.com>
2026-03-15 17:38:22 +0900

    4-1.c 제출

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
+    int a, b;
+    char c;
+    scanf("%d %d %c", &a, &b, &c);
+    int result = 0;
 
+    switch (c)
+    {
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+    }
+
+    printf("%d\n", result);
+    
     return 0;
 }
-
